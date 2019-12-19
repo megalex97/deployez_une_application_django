13d390a568988ba11cc616e3c5253eb0a17f2d5b
alex <megalex97@hotmail.com>
2019-12-19 15:19:45 +0200

    update travis config

---
+++ disquaire_project/settings/travis.py
@@ -0,0 +1,12 @@
+from . import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'disquaire',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': '',
+        'PORT': '5432',
+    },
+}
